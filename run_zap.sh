1beec64a4072a1e5e8e54a11e73a4e9bcb36e502
PSouthern <paulsouthern81@hotmail.co.uk>
2020-03-25 09:49:48 +0000

    Updating zap config

--- run_zap.sh
+++ run_zap.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sbt -Dbrowser=remote-chrome -Dlogback.configurationFile=logback.xml -Dzap.proxy=true -Dcucumber.options="--tags @unloading_remarks" 'testOnly ctc.suites.ZapSuite'
+sbt -Dbrowser=remote-chrome -Dlogback.configurationFile=logback.xml -Dzap.proxy=true 'testOnly ctc.suites.Runner'
 sbt 'testOnly ctc.suites.ZAPRunner'
