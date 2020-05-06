263968702ac01da1deace2c3f929e50e63bb2609
PSouthern <paulsouthern81@hotmail.co.uk>
2020-05-06 14:45:51 +0100

    Adding accessbility test

---
+++ src/test/scala/ctc/suites/Runner_A11y.scala
@@ -0,0 +1,42 @@
+package ctc.suites
+
+import java.io.File
+
+import ctc.driver.Driver
+import cucumber.api.CucumberOptions
+import cucumber.api.junit.Cucumber
+import org.junit.AfterClass
+import org.junit.BeforeClass
+import org.junit.runner.RunWith
+import uk.gov.hmrc.extentreport.ExtentProperties
+import uk.gov.hmrc.extentreport.Reporter
+
+@RunWith(classOf[Cucumber])
+@CucumberOptions(
+  features = Array("src/test/resources/features"),
+  glue = Array("ctc.steps"),
+  format = Array(
+    "pretty",
+    "html:target/cucumber",
+    "json:target/cucumber.json",
+    "uk.gov.hmrc.extentreport.ExtentCucumberFormatter:target/test-reports/html-report/index.html"
+  ),
+  tags = Array("@a11y")
+)
+class Runner_A11y
+
+object Runner_A11y extends Runner_A11y {
+
+  @BeforeClass
+  def setup(): Unit = {
+    val dirName          = "target/test-reports/html-report"
+    val extentProperties = ExtentProperties.INSTANCE
+    val dir              = new File(dirName)
+    val successful       = dir.mkdir()
+    ExtentProperties.create(Driver.webDriver, dirName + "/index.html")
+  }
+
+  @AfterClass
+  def writeExtentReport(): Unit =
+    Reporter.loadXMLConfig("src/test/resources/extent-config.xml")
+}
