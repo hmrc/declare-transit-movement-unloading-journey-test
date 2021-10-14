32227b240b689be43cde9175519e7fb51a30bb57
Andy Dowell <45938872+andydow311@users.noreply.github.com>
2021-10-14 15:34:59 +0100

    CTCTRADERS-2708 reverted run_a11y to remote-chrome

--- run_a11y.sh
+++ run_a11y.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-sbt -Dbrowser=chrome -Da11y=true -Denvironment=local 'testOnly ctc.suites.Runner_A11y'
+sbt -Dbrowser=remote-chrome -Da11y=true -Denvironment=local 'testOnly ctc.suites.Runner_A11y'
