b4fc7392cd045fd85924f2fe38487f455790ca33
Okeremeta Jude Tega <31142281+justega247@users.noreply.github.com>
2023-10-26 15:29:14 +0100

    Update dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,14 +3,14 @@ import sbt._
 object Dependencies {
 
   val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"         %% "webdriver-factory"  % "0.41.0",
+    "uk.gov.hmrc"         %% "webdriver-factory"  % "0.46.0",
     "org.scalatest"       %% "scalatest"          % "3.2.15",
-    "io.cucumber"         %% "cucumber-scala"     % "8.13.1",
-    "io.cucumber"          % "cucumber-junit"     % "7.10.1",
+    "io.cucumber"         %% "cucumber-scala"     % "8.14.1",
+    "io.cucumber"          % "cucumber-junit"     % "7.11.1",
     "junit"                % "junit"              % "4.13.2",
     "com.novocode"         % "junit-interface"    % "0.11",
     "com.typesafe"         % "config"             % "1.4.2",
-    "commons-io"           % "commons-io"         % "2.11.0",
+    "commons-io"           % "commons-io"         % "2.14.0",
     "com.vladsch.flexmark" % "flexmark-all"       % "0.62.2"
   ).map(_ % Test)
 
